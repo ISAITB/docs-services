baadeb99218ee91f127343c4914b34a4e277f183
simatosc <costas.simatos@sigmacubed.eu>
2025-10-02 18:01:21 +0200

    Updates for next release

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ copyright = '2025, European Commission, DIGIT'
 author = 'European Commission, DIGIT'
 
 # The short X.Y version
-version = 'GITB services version 1.27.4'
+version = 'GITB services version 1.28.0'
 # The full version, including alpha/beta/rc tags
-release = 'GITB services version 1.27.4'
+release = 'GITB services version 1.28.0'
 
 html_show_sourcelink = False
 html_show_sphinx = False
